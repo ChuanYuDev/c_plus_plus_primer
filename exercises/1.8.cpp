6fc8ed8e2cddb46f9048344b04c3e4d8854762fb
nealmatrix <nealmatrixgg@gmail.com>
2024-01-20 01:02:08 +0000

    add  1.3 to 1.9

--- exercises/1.8.cpp
+++ exercises/1.8.cpp
@@ -2,8 +2,8 @@
 
 int main()
 {
-    // std::cout << "/*";
-    // std::cout << "*/";
+    std::cout << "/*";
+    std::cout << "*/";
     // std::cout << /* "*/" */;
     std::cout << /* "*/" /* "/*" */;
     return 0;
