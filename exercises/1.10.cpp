447ffbc37ea66bb6192a0175330be03ceb91baf9
nealmatrix <nealmatrixgg@gmail.com>
2024-01-22 15:08:44 +0000

    add 1.10 1.11 1.12 1.13

--- exercises/1.10.cpp
+++ exercises/1.10.cpp
@@ -4,7 +4,8 @@ int main()
 {
     int val = 10;
 
-    while (val >= 0){
+    while (val >= 0)
+    {
         std::cout << val << std::endl;
         --val;
     }
--- exercises/1.11.cpp
+++ exercises/1.11.cpp
@@ -9,14 +9,18 @@ int main()
 
     std::cout << "All the numbers between the range specified by these two integers are: " << std::endl;
 
-    if (v1 > v2){
-        while (v1 >= v2){
+    if (v1 > v2)
+    {
+        while (v1 >= v2)
+        {
             std::cout << v1 << std::endl;
             --v1;
         }
     }
-    else {
-        while (v1 <= v2){
+    else 
+    {
+        while (v1 <= v2)
+        {
             std::cout << v1 << std::endl;
             ++v1;
         }
