89b227b43a11efb5697a3dccf916538a4710d397
nealmatrix <nealmatrixgg@gmail.com>
2024-03-04 08:34:02 +0000

    add tests sample

---
+++ tests/sample.cpp
@@ -0,0 +1,10 @@
+#define BOOST_TEST_MODULE test
+
+#include <boost/test/unit_test.hpp>
+#include <iostream>
+
+BOOST_AUTO_TEST_SUITE(test_opeartor_precedence)
+BOOST_AUTO_TEST_CASE(test_0)
+{
+}
+BOOST_AUTO_TEST_SUITE_END()
